f61c4e75b843377e4d1ca3f7c33adba5d7f54499
Adrien CALMESNIL <acalmesnil@consultants-solutec.fr>
2024-03-05 11:38:18 +0100

    first commit

---
+++ backend/movies/src/main/java/dev/adrien/movies/moviesFiles/ReviewRepository.java
@@ -0,0 +1,10 @@
+package dev.adrien.movies.moviesFiles;
+
+import dev.adrien.movies.moviesFiles.Review;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
